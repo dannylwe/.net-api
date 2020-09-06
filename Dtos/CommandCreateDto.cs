d3ec820f8a2c366be99d4e8623351420c0693255
Daniel Lwetabe <acehanks@gmail.com>
2020-09-07 00:45:46 +0300

    add annotations to create DTO to provide user clear errors

--- Dtos/CommandCreateDto.cs
+++ Dtos/CommandCreateDto.cs
@@ -5,10 +5,13 @@ namespace Commander.Dtos
     public class CommandCreateDto
     {
        
+        [Required]
         public string HowTo { get; set; }
 
+        [Required]
         public string Line { get; set; }
         
+        [Required]
         public string Platform { get; set; }
     }
 }
